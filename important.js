01150ccaa6105f4c0859e26bdfe835af2a6c76d7
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-09-11 15:39:11 +0530

    "doing js exercise"

--- important.js
+++ important.js
@@ -1,8 +1,8 @@
-// lets see some amazing things 
+// lets see some amazing things
 //  console.log(0=='');
 //   console.log(3===3);
 //    console.log(3==3);
-//      console.log(3=='3');// checks only value not data type 
+//      console.log(3=='3');// checks only value not data type
 
 const { forwardRef } = require("react");
 
@@ -30,8 +30,6 @@ const { forwardRef } = require("react");
 
 // console.log(`${date}/${month}/${year} ${hours}:${minutes}`) // 4/1/2020 0:56
 
-
-
 // Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
 
 // Enter birth year: 1995
@@ -44,7 +42,7 @@ const { forwardRef } = require("react");
 // let age = now.getFullYear();
 // let newo = age - reqage;
 // console.log(newo > 18 ? "ok man" : "under age");
- 
+
 // const arr=[1,2,3,4]
 //  console.log(arr);
 //   const need = [
@@ -75,8 +73,7 @@ const { forwardRef } = require("react");
 //    for (const elem of ages) {
 // sum+=elem;
 //    }
-//    console.log(sum / ages.length);  
-
+//    console.log(sum / ages.length);
 
 // Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array
 
@@ -85,18 +82,16 @@ const { forwardRef } = require("react");
 // for (let i = 0; i <=100; i++) {
 //    const element =i;
 //     if(element%2==0)
-// { 
+// {
 //     even += element;
 
 // }else{
 //    odd+=element;
 // }
-  
-   
+
 // }
 //  console.log(even);
 //   console.log(odd);
-
 
 // console.log(numbers.includes(5)) // true
 
@@ -119,10 +114,8 @@ const { forwardRef } = require("react");
 //    {
 //       console.log(elem);
 //    }
-   
+
 //  }
-
-
 
 //   const fullStack = [
 //     ["HTML", "CSS", "JS", "React"],
@@ -132,217 +125,212 @@ const { forwardRef } = require("react");
 //    for (let i = 0; i < fullStack.length; i++) {
 //      const element = fullStack[i];
 //      for (const elem of element) {
-//        console.log(elem+"\n"); 
+//        console.log(elem+"\n");
 //      }
-    
+
 //    }
 
+const countries = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cape Verde",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombi",
+  "Comoros",
+  "Congo (Brazzaville)",
+  "Congo",
+  "Costa Rica",
+  "Cote d'Ivoire",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "East Timor (Timor Timur)",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Ethiopia",
+  "Fiji",
+  "Finland",
+  "France",
+  "Gabon",
+  "Gambia, The",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Korea, North",
+  "Korea, South",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macedonia",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Qatar",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia and Montenegro",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Vatican City",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
 
-
-   const countries = [
-     "Afghanistan",
-     "Albania",
-     "Algeria",
-     "Andorra",
-     "Angola",
-     "Antigua and Barbuda",
-     "Argentina",
-     "Armenia",
-     "Australia",
-     "Austria",
-     "Azerbaijan",
-     "Bahamas",
-     "Bahrain",
-     "Bangladesh",
-     "Barbados",
-     "Belarus",
-     "Belgium",
-     "Belize",
-     "Benin",
-     "Bhutan",
-     "Bolivia",
-     "Bosnia and Herzegovina",
-     "Botswana",
-     "Brazil",
-     "Brunei",
-     "Bulgaria",
-     "Burkina Faso",
-     "Burundi",
-     "Cambodia",
-     "Cameroon",
-     "Canada",
-     "Cape Verde",
-     "Central African Republic",
-     "Chad",
-     "Chile",
-     "China",
-     "Colombi",
-     "Comoros",
-     "Congo (Brazzaville)",
-     "Congo",
-     "Costa Rica",
-     "Cote d'Ivoire",
-     "Croatia",
-     "Cuba",
-     "Cyprus",
-     "Czech Republic",
-     "Denmark",
-     "Djibouti",
-     "Dominica",
-     "Dominican Republic",
-     "East Timor (Timor Timur)",
-     "Ecuador",
-     "Egypt",
-     "El Salvador",
-     "Equatorial Guinea",
-     "Eritrea",
-     "Estonia",
-     "Ethiopia",
-     "Fiji",
-     "Finland",
-     "France",
-     "Gabon",
-     "Gambia, The",
-     "Georgia",
-     "Germany",
-     "Ghana",
-     "Greece",
-     "Grenada",
-     "Guatemala",
-     "Guinea",
-     "Guinea-Bissau",
-     "Guyana",
-     "Haiti",
-     "Honduras",
-     "Hungary",
-     "Iceland",
-     "India",
-     "Indonesia",
-     "Iran",
-     "Iraq",
-     "Ireland",
-     "Israel",
-     "Italy",
-     "Jamaica",
-     "Japan",
-     "Jordan",
-     "Kazakhstan",
-     "Kenya",
-     "Kiribati",
-     "Korea, North",
-     "Korea, South",
-     "Kuwait",
-     "Kyrgyzstan",
-     "Laos",
-     "Latvia",
-     "Lebanon",
-     "Lesotho",
-     "Liberia",
-     "Libya",
-     "Liechtenstein",
-     "Lithuania",
-     "Luxembourg",
-     "Macedonia",
-     "Madagascar",
-     "Malawi",
-     "Malaysia",
-     "Maldives",
-     "Mali",
-     "Malta",
-     "Marshall Islands",
-     "Mauritania",
-     "Mauritius",
-     "Mexico",
-     "Micronesia",
-     "Moldova",
-     "Monaco",
-     "Mongolia",
-     "Morocco",
-     "Mozambique",
-     "Myanmar",
-     "Namibia",
-     "Nauru",
-     "Nepal",
-     "Netherlands",
-     "New Zealand",
-     "Nicaragua",
-     "Niger",
-     "Nigeria",
-     "Norway",
-     "Oman",
-     "Pakistan",
-     "Palau",
-     "Panama",
-     "Papua New Guinea",
-     "Paraguay",
-     "Peru",
-     "Philippines",
-     "Poland",
-     "Portugal",
-     "Qatar",
-     "Romania",
-     "Russia",
-     "Rwanda",
-     "Saint Kitts and Nevis",
-     "Saint Lucia",
-     "Saint Vincent",
-     "Samoa",
-     "San Marino",
-     "Sao Tome and Principe",
-     "Saudi Arabia",
-     "Senegal",
-     "Serbia and Montenegro",
-     "Seychelles",
-     "Sierra Leone",
-     "Singapore",
-     "Slovakia",
-     "Slovenia",
-     "Solomon Islands",
-     "Somalia",
-     "South Africa",
-     "Spain",
-     "Sri Lanka",
-     "Sudan",
-     "Suriname",
-     "Swaziland",
-     "Sweden",
-     "Switzerland",
-     "Syria",
-     "Taiwan",
-     "Tajikistan",
-     "Tanzania",
-     "Thailand",
-     "Togo",
-     "Tonga",
-     "Trinidad and Tobago",
-     "Tunisia",
-     "Turkey",
-     "Turkmenistan",
-     "Tuvalu",
-     "Uganda",
-     "Ukraine",
-     "United Arab Emirates",
-     "United Kingdom",
-     "United States",
-     "Uruguay",
-     "Uzbekistan",
-     "Vanuatu",
-     "Vatican City",
-     "Venezuela",
-     "Vietnam",
-     "Yemen",
-     "Zambia",
-     "Zimbabwe",
-   ];
-
-
-
-   // toUpperCase();
+// toUpperCase();
 
 //   for (let i = 0; i < countries.length; i++) {
 //   countries[i]= countries[i].toUpperCase();
 
-   
 //   }
 //      console.log("\n");
 
@@ -350,14 +338,10 @@ const { forwardRef } = require("react");
 //     for (const elem of countries) {
 //       console.log(elem);
 //     }
-     
-
-
-
 
 // The following is an array of 10 students ages:
 
-const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 // Sort the array and find the min and max age
 // Find the median age(one middle item or two middle items divided by two)
 // Find the average age(all items divided by number of items)
@@ -367,7 +351,7 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 // ages.sort();
 //  for (const elem of ages) {
 //     console.log(elem);
-   
+
 //  }
 //  console.log(ages[0]);
 
@@ -376,13 +360,7 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 //    let range = ages[ages.length-1]-ages[0];
 //     console.log(range);
 
-
 // slice 10 countries fron countries Array
-
-
-
-
-
 
 // const newarr = [ 1,2,3,4,5,6];
 //  console.log(newarr.length);
@@ -399,10 +377,10 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 // modify Tea to 'Green Tea'
 
 const shoppingCart = [];
- shoppingCart.push("Milk");
-  shoppingCart.push("Coffee");
-   shoppingCart.push("Tea");
-    shoppingCart.push("Honey");
+shoppingCart.push("Milk");
+shoppingCart.push("Coffee");
+shoppingCart.push("Tea");
+shoppingCart.push("Honey");
 
 //      for (const elem of shoppingCart) {
 //        console.log(elem);
@@ -411,30 +389,28 @@ const shoppingCart = [];
 //      if(shoppingCart.indexOf("Meat")==-1)
 //      {
 // shoppingCart.unshift("Meat");
-// unshift means add 
-    //  }
-     
-     
-    //  for (const elem of shoppingCart) {
-    //    console.log(elem);
-    //  }
+// unshift means add
+//  }
 
+//  for (const elem of shoppingCart) {
+//    console.log(elem);
+//  }
 
-    //   console.log("\n");
-     
-    //  if (shoppingCart.indexOf("Sugar") == -1) {
-    //    shoppingCart.push("sugar")
-    //    // unshift means add
-    //  }
-    //     for (const elem of shoppingCart) {
-    //       console.log(elem);
-    //     }
+//   console.log("\n");
 
-      // i am alergic of honey 
-       
+//  if (shoppingCart.indexOf("Sugar") == -1) {
+//    shoppingCart.push("sugar")
+//    // unshift means add
+//  }
+//     for (const elem of shoppingCart) {
+//       console.log(elem);
+//     }
+
+// i am alergic of honey
+
 //     const check =shoppingCart.indexOf("Honey");
 //     shoppingCart.splice(check,1);
-     
+
 //      console.log("\n");
 //         for (const elem of shoppingCart) {
 //           console.log(elem);
@@ -447,62 +423,47 @@ const shoppingCart = [];
 //           console.log(elem);
 //         }
 
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
 
+// for (let i = 0; i <7 ; i++) {
+//   for (let j = 0; j < i+1; j++) {
+//     console.log("#");
+//   }
+//   console.log("\n");
 
-    // #
-    // ##
-    // ###
-    // ####
-    // #####
-    // ######
-    // #######
+// }
 
+// Call your function sum, it takes any number of arguments and it returns the sum.
 
+//    const allsum = (...arr)=>
+//    {
+//     let sum=0;
+//     for (const iterator of arr) {
 
+//       sum+=iterator;      }
+//        return sum;
+//    }
 
-  
-    // for (let i = 0; i <7 ; i++) {
-    //   for (let j = 0; j < i+1; j++) {
-    //     console.log("#");
-    //   }
-    //   console.log("\n");
-      
-    // }
+//  let si=  allsum(1,2,2,3,4);
+//     console.log(si);
 
-
-    // Call your function sum, it takes any number of arguments and it returns the sum.
-
-
-  //    const allsum = (...arr)=>
-  //    {
-  //     let sum=0;
-  //     for (const iterator of arr) {
-         
-  //       sum+=iterator;      }
-  //        return sum;
-  //    }
-
-  //  let si=  allsum(1,2,2,3,4);
-  //     console.log(si);
-
-
-
-
-    //  const sum=()=>{
-    //  return   Math.floor( Math.random()*100);
-    //  }
-    //  console.log(sum()); 
-
-
-
-
+//  const sum=()=>{
+//  return   Math.floor( Math.random()*100);
+//  }
+//  console.log(sum());
 
 //Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
 
 //  const arr =[];
 
 //  const additem = (elem )=>{
-   
+
 //   arr.push(elem);
 
 //    return arr;
@@ -512,17 +473,47 @@ const shoppingCart = [];
 
 //   for (const elem of newarr) {
 //      console.log(elem);
-    
+
 //   }
 
-    
+//  const reverseArray=(...arr)=>{
 
- const reverseArray=(...arr)=>{
-   
-  reverseArray(arr);
-   return arr;
- }
+//   reverseArray(arr);
+//    return arr;
+//  }
 
+// console.log(reverseArray([1, 2, 3, 4, 5]));
 
+// Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
 
-console.log(reverseArray([1, 2, 3, 4, 5]));
+// const convertoctoof=(oc)=>{
+
+//  const oF = (oc * 9/5) + 32.
+//   return oF;
+
+// }
+
+//  const the_of= convertoctoof(10);
+//  console.log(the_of);
+
+// Write a functions which checks if all items are unique in the array.
+
+const jsunique = () => {
+
+  let arr = [1, 2, 3, 5, 6];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i != j) {
+        console.log(arr[i] + ".-"+arr[j]);
+
+        if (arr[i] == arr[j]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+const need = jsunique();
+console.log(need);
